98f15ea2f06f8143f209d6e1c380f8aa35af5d0c
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2014-11-30 16:46:03 +0100

    modified file names to relative for them to work on lacalgpu2.

--- common/files.hpp
+++ common/files.hpp
@@ -3,30 +3,30 @@
 
 #define NUMBER_OF_FILES 12
 
-const char* src_file_names[NUMBER_OF_FILES] = {"/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/avatar.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/cameleon.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/frozen.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/giraffe.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/snow_leopard.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/tmnt_1.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/tmnt_2.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/transformers.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/tree_1.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/tree_2.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/tree_3.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/binary/zebra.bmp"};
+const char* src_file_names[NUMBER_OF_FILES] = {"../../../../images/binary/avatar.bmp",
+                                               "../../../../images/binary/cameleon.bmp",
+                                               "../../../../images/binary/frozen.bmp",
+                                               "../../../../images/binary/giraffe.bmp",
+                                               "../../../../images/binary/snow_leopard.bmp",
+                                               "../../../../images/binary/tmnt_1.bmp",
+                                               "../../../../images/binary/tmnt_2.bmp",
+                                               "../../../../images/binary/transformers.bmp",
+                                               "../../../../images/binary/tree_1.bmp",
+                                               "../../../../images/binary/tree_2.bmp",
+                                               "../../../../images/binary/tree_3.bmp",
+                                               "../../../../images/binary/zebra.bmp"};
 
-const char* dst_file_names[NUMBER_OF_FILES] = {"/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/avatar.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/cameleon.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/frozen.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/giraffe.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/snow_leopard.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/tmnt_1.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/tmnt_2.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/transformers.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/tree_1.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/tree_2.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/tree_3.bmp",
-                                               "/home/sahand/Documents/EPFL/Courses/Master/MA1/Program Parallelization on PC Clusters/Project/images/skeletonized/zebra.bmp"};
+const char* dst_file_names[NUMBER_OF_FILES] = {"../../../../images/skeletonized/avatar.bmp",
+                                               "../../../../images/skeletonized/cameleon.bmp",
+                                               "../../../../images/skeletonized/frozen.bmp",
+                                               "../../../../images/skeletonized/giraffe.bmp",
+                                               "../../../../images/skeletonized/snow_leopard.bmp",
+                                               "../../../../images/skeletonized/tmnt_1.bmp",
+                                               "../../../../images/skeletonized/tmnt_2.bmp",
+                                               "../../../../images/skeletonized/transformers.bmp",
+                                               "../../../../images/skeletonized/tree_1.bmp",
+                                               "../../../../images/skeletonized/tree_2.bmp",
+                                               "../../../../images/skeletonized/tree_3.bmp",
+                                               "../../../../images/skeletonized/zebra.bmp"};
 
 #endif
