d488267ce0c51a1718ac8c1ddc5d7d8d64f45e18
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2014-12-02 22:51:02 +0100

    moved non-skeletonize code to main for gpu1

--- gpu1/gpu1.cu
+++ gpu1/gpu1.cu
@@ -42,92 +42,43 @@ __device__ uint8_t black_neighbors_around(uint8_t* d_data, unsigned int row, uns
     return count;
 }
 
-// Performs an image skeletonization algorithm on the input file, and stores the
-// result in the specified output file.
-unsigned int skeletonize(const char* src_fname, const char* dst_fname) {
-    assert(src_fname && "src_fname must be non-NULL");
-    assert(dst_fname && "dst_fname must be non-NULL");
-
-    // load src image
-    Bitmap* src_bitmap = loadBitmap(src_fname);
-    assert(src_bitmap && "Bitmap has to be non-NULL");
-
-    // validate src image is 8-bit binary-valued grayscale image
-    assert(is_binary_valued_grayscale_image(src_bitmap) && "Only 8-bit binary-valued grayscale images are supported");
-
-    // we work on true binary images
-    grayscale_to_binary(src_bitmap);
-
-    // Create dst bitmap image (empty for now)
-    Bitmap* dst_bitmap = createBitmap(src_bitmap->width, src_bitmap->height, src_bitmap->depth);
-
-    // Dimensions of computing elements on the CUDA device.
-    // Computing the grid dimensions depends on PAD_TOP and PAD_LEFT.
-    unsigned int block_dim_x = THREADS_PER_BLOCK_X;
-    unsigned int block_dim_y = THREADS_PER_BLOCK_Y;
-    unsigned int grid_dim_x = (unsigned int) ceil((src_bitmap->width) / ((double) block_dim_x));
-    unsigned int grid_dim_y = (unsigned int) ceil((src_bitmap->height)/ ((double) block_dim_y));
-    dim3 block_dim(block_dim_x, block_dim_y);
-    dim3 grid_dim(grid_dim_x, grid_dim_y);
-
-    // Pad the binary images with pixels on each side. This will be useful when
-    // implementing the skeletonization algorithm, because the mask we use
-    // depends on P2 and P4, which also have their own window.
-    // ATTENTION : it is important to use cast to (int) since we want to test
-    // for a maximum value and the subtraction can yield a negative number.
-    Padding padding;
-    padding.top = PAD_TOP;
-    padding.bottom = max((int) ((grid_dim_y * block_dim_y) - (src_bitmap->height + PAD_BOTTOM)), PAD_BOTTOM);
-    padding.left = PAD_LEFT;
-    padding.right = max((int) ((grid_dim_x * block_dim_x) - (src_bitmap->width + PAD_RIGHT)), PAD_RIGHT);
-    pad_binary_bitmap(&src_bitmap, BINARY_WHITE, padding);
-    pad_binary_bitmap(&dst_bitmap, BINARY_WHITE, padding);
-
+// Performs an image skeletonization algorithm on the input Bitmap, and stores
+// the result in the output Bitmap.
+unsigned int skeletonize(Bitmap** src_bitmap, Bitmap** dst_bitmap, Padding padding, dim3 grid_dim, dim3 block_dim) {
     // allocate memory on device
     uint8_t* d_src_data = NULL;
     uint8_t* d_dst_data = NULL;
-    unsigned int data_size = src_bitmap->width * src_bitmap->height * sizeof(uint8_t);
+    unsigned int data_size = (*src_bitmap)->width * (*src_bitmap)->height * sizeof(uint8_t);
     cudaError d_src_malloc_success = cudaMalloc((void**) &d_src_data, data_size);
     cudaError d_dst_malloc_success = cudaMalloc((void**) &d_dst_data, data_size);
-    assert((d_src_malloc_success == cudaSuccess) && "Could not allocate memory for d_src_data");
-    assert((d_dst_malloc_success == cudaSuccess) && "Could not allocate memory for d_dst_data");
+    assert((d_src_malloc_success == cudaSuccess) && "Error: could not allocate memory for d_src_data");
+    assert((d_dst_malloc_success == cudaSuccess) && "Error: could not allocate memory for d_dst_data");
 
     // send data to device
-    cudaMemcpy(d_src_data, src_bitmap->data, data_size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_dst_data, dst_bitmap->data, data_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_src_data, (*src_bitmap)->data, data_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_dst_data, (*dst_bitmap)->data, data_size, cudaMemcpyHostToDevice);
 
-    // iterative thinning algorithm
     unsigned int iterations = 0;
     do {
-        skeletonize_pass<<<grid_dim, block_dim>>>(d_src_data, d_dst_data, src_bitmap->width, src_bitmap->height, padding);
+        skeletonize_pass<<<grid_dim, block_dim>>>(d_src_data, d_dst_data, (*src_bitmap)->width, (*src_bitmap)->height, padding);
 
         // bring data back from device
-        cudaMemcpy(src_bitmap->data, d_src_data, data_size, cudaMemcpyDeviceToHost);
-        cudaMemcpy(dst_bitmap->data, d_dst_data, data_size, cudaMemcpyDeviceToHost);
+        cudaMemcpy((*src_bitmap)->data, d_src_data, data_size, cudaMemcpyDeviceToHost);
+        cudaMemcpy((*dst_bitmap)->data, d_dst_data, data_size, cudaMemcpyDeviceToHost);
 
         swap_bitmaps((void**) &d_src_data, (void**) &d_dst_data);
 
         iterations++;
         printf(".");
         fflush(stdout);
-    } while (!are_identical_bitmaps(src_bitmap, dst_bitmap));
+    } while (!are_identical_bitmaps(*src_bitmap, *dst_bitmap));
 
-    cudaMemcpy(dst_bitmap->data, d_dst_data, data_size, cudaMemcpyDeviceToHost);
+    // TODO : may not be needed
+    cudaMemcpy((*dst_bitmap)->data, d_dst_data, data_size, cudaMemcpyDeviceToHost);
 
-    // Remove extra padding that was added to the images (don't care about
-    // src_bitmap, so only need to unpad dst_bitmap)
-    unpad_binary_bitmap(&dst_bitmap, padding);
-
-    // save 8-bit binary-valued grayscale version of dst_bitmap to dst_fname
-    binary_to_grayscale(dst_bitmap);
-    int save_successful = saveBitmap(dst_fname, dst_bitmap);
-    assert(save_successful && "Could not save dst_bitmap");
-
-    // deallocate memory used for bitmaps
+    // free memory on device
     cudaFree(d_src_data);
     cudaFree(d_dst_data);
-    free(src_bitmap);
-    free(dst_bitmap);
 
     return iterations;
 }
@@ -171,24 +122,66 @@ __device__ uint8_t wb_transitions_around(uint8_t* d_data, unsigned int row, unsi
     return count;
 }
 
-int main(void) {
-    for (unsigned int i = 0; i < NUMBER_OF_FILES; i++) {
-        int file_name_len = strlen(src_file_names[i]);
-        char* file_name = (char*) calloc(file_name_len + 1, sizeof(char));
-        strncpy(file_name, src_file_names[i], file_name_len);
-        char* base_file_name = basename(file_name);
-        printf("%s ", base_file_name);
-        fflush(stdout);
-        free(file_name);
+int main(int argc, char** argv) {
+    assert(argc == 3 && "Usage: gpu1 <input_file_name.bmp> <output_file_name.bmp>");
 
-        unsigned int iterations = skeletonize(src_file_names[i], dst_file_names[i]);
+    char* src_fname = argv[1];
+    char* dst_fname = argv[2];
 
-        printf(" %d iterations\n", iterations);
-        fflush(stdout);
-    }
+    printf("src_fname = %s\n", src_fname);
+    printf("dst_fname = %s\n", dst_fname);
 
-    printf("done\n");
-    fflush(stdout);
+    // load src image
+    Bitmap* src_bitmap = loadBitmap(src_fname);
+    assert(src_bitmap != NULL && "Error: could not load src bitmap");
 
-    return 0;
+    // validate src image is 8-bit binary-valued grayscale image
+    assert(is_binary_valued_grayscale_image(src_bitmap) && "Error: Only 8-bit binary-valued grayscale images are supported. Values must be black (0) or white (255) only");
+
+    // we work on true binary images
+    grayscale_to_binary(src_bitmap);
+
+    // Create dst bitmap image (empty for now)
+    Bitmap* dst_bitmap = createBitmap(src_bitmap->width, src_bitmap->height, src_bitmap->depth);
+    assert(dst_bitmap != NULL && "Error: could not allocate memory for dst bitmap");
+
+    // Dimensions of computing elements on the CUDA device.
+    // Computing the grid dimensions depends on PAD_TOP and PAD_LEFT.
+    unsigned int block_dim_x = THREADS_PER_BLOCK_X;
+    unsigned int block_dim_y = THREADS_PER_BLOCK_Y;
+    unsigned int grid_dim_x = (unsigned int) ceil((src_bitmap->width) / ((double) block_dim_x));
+    unsigned int grid_dim_y = (unsigned int) ceil((src_bitmap->height)/ ((double) block_dim_y));
+    dim3 block_dim(block_dim_x, block_dim_y);
+    dim3 grid_dim(grid_dim_x, grid_dim_y);
+
+    // Pad the binary images with pixels on each side. This will be useful when
+    // implementing the skeletonization algorithm, because the mask we use
+    // depends on P2 and P4, which also have their own window.
+    // ATTENTION : it is important to use cast to (int) since we want to test
+    // for a maximum value and the subtraction can yield a negative number.
+    Padding padding;
+    padding.top = PAD_TOP;
+    padding.bottom = max((int) ((grid_dim_y * block_dim_y) - (src_bitmap->height + PAD_BOTTOM)), PAD_BOTTOM);
+    padding.left = PAD_LEFT;
+    padding.right = max((int) ((grid_dim_x * block_dim_x) - (src_bitmap->width + PAD_RIGHT)), PAD_RIGHT);
+    pad_binary_bitmap(&src_bitmap, BINARY_WHITE, padding);
+    pad_binary_bitmap(&dst_bitmap, BINARY_WHITE, padding);
+
+    unsigned int iterations = skeletonize(&src_bitmap, &dst_bitmap, padding, grid_dim, block_dim);
+    printf(" %u iterations\n", iterations);
+
+    // Remove extra padding that was added to the images (don't care about
+    // src_bitmap, so only need to unpad dst_bitmap)
+    unpad_binary_bitmap(&dst_bitmap, padding);
+
+    // save 8-bit binary-valued grayscale version of dst_bitmap to dst_fname
+    binary_to_grayscale(dst_bitmap);
+    int save_successful = saveBitmap(dst_fname, dst_bitmap);
+    assert(save_successful == 1 && "Error: could not save dst bitmap");
+
+    // deallocate memory used for bitmaps
+    free(src_bitmap);
+    free(dst_bitmap);
+
+    return EXIT_SUCCESS;
 }
--- gpu1/gpu1.cuh
+++ gpu1/gpu1.cuh
@@ -5,7 +5,7 @@
 #include "../common/utils.hpp"
 
 __device__ uint8_t black_neighbors_around(uint8_t* data, unsigned int row, unsigned int col, unsigned int width);
-unsigned int skeletonize(const char* src_fname, const char* dst_fname);
+unsigned int skeletonize(Bitmap** src_bitmap, Bitmap** dst_bitmap, Padding padding, dim3 grid_dim, dim3 block_dim);
 __global__ void skeletonize_pass(uint8_t* src, uint8_t* dst, unsigned int width, unsigned int height, Padding padding);
 __device__ uint8_t wb_transitions_around(uint8_t* data, unsigned int row, unsigned int col, unsigned int width);
 
