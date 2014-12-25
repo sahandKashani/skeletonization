11c926ba7303dc2587b9f66a8d07930af338ce8e
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2014-12-25 15:02:54 +0100

    changed compilation phase to include code for most cuda architectures

--- gpu2/compile.sh
+++ gpu2/compile.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-nvcc --ptxas-options=-v -arch=sm_20 gpu2.cu ../common/gpu_only_utils.cu ../common/lspbmp.cpp ../common/utils.cpp -o ../../bin/gpu2 --compiler-options "-g -O3"
+nvcc \
+--ptxas-options=-v \
+-gencode=arch=compute_10,code=sm_10 \
+-gencode=arch=compute_20,code=sm_20 \
+-gencode=arch=compute_30,code=sm_30 \
+-gencode=arch=compute_35,code=sm_35 \
+-gencode=arch=compute_35,code=compute_35 \
+gpu2.cu ../common/gpu_only_utils.cu ../common/lspbmp.cpp ../common/utils.cpp \
+-O3 \
+-o ../../bin/gpu2 \
+--compiler-options "-g -O3"
--- gpu3/compile.sh
+++ gpu3/compile.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-nvcc --ptxas-options=-v -arch=sm_20 gpu3.cu ../common/gpu_only_utils.cu ../common/lspbmp.cpp ../common/utils.cpp -o ../../bin/gpu3 --compiler-options "-g -O3"
+nvcc \
+--ptxas-options=-v \
+-gencode=arch=compute_10,code=sm_10 \
+-gencode=arch=compute_20,code=sm_20 \
+-gencode=arch=compute_30,code=sm_30 \
+-gencode=arch=compute_35,code=sm_35 \
+-gencode=arch=compute_35,code=compute_35 \
+gpu3.cu ../common/gpu_only_utils.cu ../common/lspbmp.cpp ../common/utils.cpp \
+-O3 \
+-o ../../bin/gpu3 \
+--compiler-options "-g -O3"
