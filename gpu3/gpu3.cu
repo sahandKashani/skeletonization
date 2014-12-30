7afc7b01e4f710bbf5e508cb7a5bc3a23c687902
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2014-12-30 18:15:59 +0100

    loading into shared memory done

--- gpu3/gpu3.cu
+++ gpu3/gpu3.cu
@@ -233,15 +233,25 @@ __global__ void skeletonize_pass(uint8_t* d_src, uint8_t* d_dst, int width, int 
         s_src[(s_row) * s_width + (s_col + 1)] = global_mem_read(d_src, d_row, d_col + 1, width, height);
     } else if (threadIdx.y == 0) {
         // PAD_TOP top rows
+        s_src[(s_row - 2) * s_width + (s_col)] = global_mem_read(d_src, d_row - 2, d_col, width, height);
+        s_src[(s_row - 1) * s_width + (s_col)] = global_mem_read(d_src, d_row - 1, d_col, width, height);
+        s_src[(s_row) * s_width + (s_col)] = global_mem_read(d_src, d_row, d_col, width, height);
     } else if (threadIdx.x == 0) {
         // PAD_LEFT left rows
+        s_src[(s_row) * s_width + (s_col - 2)] = global_mem_read(d_src, d_row, d_col - 2, width, height);
+        s_src[(s_row) * s_width + (s_col - 1)] = global_mem_read(d_src, d_row, d_col - 1, width, height);
+        s_src[(s_row) * s_width + (s_col)] = global_mem_read(d_src, d_row, d_col, width, height);
     } else if (threadIdx.y == (blockDim.y - 1)) {
         // PAD_BOTTOM bottom rows
+        s_src[(s_row) * s_width + (s_col)] = global_mem_read(d_src, d_row, d_col, width, height);
+        s_src[(s_row + 1) * s_width + (s_col)] = global_mem_read(d_src, d_row + 1, d_col, width, height);
     } else if (threadIdx.x == (blockDim.x - 1)) {
         // PAD_RIGHT right rows
+        s_src[(s_row) * s_width + (s_col)] = global_mem_read(d_src, d_row, d_col, width, height);
+        s_src[(s_row) * s_width + (s_col + 1)] = global_mem_read(d_src, d_row, d_col + 1, width, height);
     } else {
         // center pixels
-        s_src[s_row * s_width + s_col] = global_mem_read(d_src, d_row, d_col, width, height);
+        s_src[(s_row) * s_width + (s_col)] = global_mem_read(d_src, d_row, d_col, width, height);
     }
 
     __syncthreads();
