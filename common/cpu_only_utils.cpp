1a4aa43f5151b15ee7451ffe49ec746d1ea90066
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2014-12-30 05:05:16 +0100

    adding parenthesis for security

--- common/cpu_only_utils.cpp
+++ common/cpu_only_utils.cpp
@@ -11,7 +11,7 @@ void cpu_post_skeletonization(char** argv, Bitmap** src_bitmap, Bitmap** dst_bit
     // save 8-bit binary-valued grayscale version of dst_bitmap to dst_fname
     binary_to_grayscale(*dst_bitmap);
     int save_successful = saveBitmap(dst_fname, *dst_bitmap);
-    assert(save_successful == 1 && "Error: could not save dst_bitmap");
+    assert((save_successful == 1) && "Error: could not save dst_bitmap");
 
     // free memory used for bitmaps
     free(*src_bitmap);
@@ -19,7 +19,7 @@ void cpu_post_skeletonization(char** argv, Bitmap** src_bitmap, Bitmap** dst_bit
 }
 
 void cpu_pre_skeletonization(int argc, char** argv, Bitmap** src_bitmap, Bitmap** dst_bitmap) {
-    assert(argc == 3 && "Usage: ./<cpu_binary> <input_file_name.bmp> <output_file_name.bmp>");
+    assert((argc == 3) && "Usage: ./<cpu_binary> <input_file_name.bmp> <output_file_name.bmp>");
 
     char* src_fname = argv[1];
     char* dst_fname = argv[2];
@@ -29,7 +29,7 @@ void cpu_pre_skeletonization(int argc, char** argv, Bitmap** src_bitmap, Bitmap*
 
     // load src image
     *src_bitmap = loadBitmap(src_fname);
-    assert(*src_bitmap != NULL && "Error: could not load src_bitmap");
+    assert((*src_bitmap != NULL) && "Error: could not load src_bitmap");
 
     // validate src image is 8-bit binary-valued grayscale image
     assert(is_binary_valued_grayscale_image(*src_bitmap) && "Error: Only 8-bit binary-valued grayscale images are supported. Values must be black (0) or white (255) only");
@@ -39,7 +39,7 @@ void cpu_pre_skeletonization(int argc, char** argv, Bitmap** src_bitmap, Bitmap*
 
     // Create dst bitmap image (empty for now)
     *dst_bitmap = createBitmap((*src_bitmap)->width, (*src_bitmap)->height, (*src_bitmap)->depth);
-    assert(*dst_bitmap != NULL && "Error: could not allocate memory for dst_bitmap");
+    assert((*dst_bitmap != NULL) && "Error: could not allocate memory for dst_bitmap");
 
     printf("image information\n");
     printf("=================\n");
