96828fa92b908ee9ad0d16147d09d49c7b802347
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2014-12-30 04:21:04 +0100

    gpu1 works with padding.

--- common/gpu_only_utils.cuh
+++ common/gpu_only_utils.cuh
@@ -14,7 +14,14 @@ inline void gpuAssert(cudaError_t code, const char* file, int line) {
     }
 }
 
-void gpu_post_skeletonization(char** argv, Bitmap** src_bitmap, Bitmap** dst_bitmap);
-void gpu_pre_skeletonization(int argc, char** argv, Bitmap** src_bitmap, Bitmap** dst_bitmap, dim3* grid_dim, dim3* block_dim);
+typedef struct {
+    int bottom;
+    int right;
+} Padding;
+
+void gpu_post_skeletonization(char** argv, Bitmap** src_bitmap, Bitmap** dst_bitmap, Padding* padding);
+void gpu_pre_skeletonization(int argc, char** argv, Bitmap** src_bitmap, Bitmap** dst_bitmap, Padding* padding, dim3* grid_dim, dim3* block_dim);
+void pad_binary_bitmap(Bitmap** image, uint8_t binary_padding_value, Padding padding);
+void unpad_binary_bitmap(Bitmap** image, Padding padding);
 
 #endif
