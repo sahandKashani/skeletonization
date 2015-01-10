70a65fcef91b60f15337329ee0a3d0e594b691b2
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-01-10 14:25:41 +0100

    sm 1.0 is deprecated

--- gpu2/compile.sh
+++ gpu2/compile.sh
@@ -6,7 +6,6 @@ cd "$DIR"
 nvcc \
 --generate-line-info \
 --ptxas-options=-v \
--gencode=arch=compute_10,code=sm_10 \
 -gencode=arch=compute_20,code=sm_20 \
 -gencode=arch=compute_30,code=sm_30 \
 -gencode=arch=compute_35,code=sm_35 \
