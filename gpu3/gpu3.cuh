849d004cf6b2878e234f4f578f6788a1e0140b0a
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-01-10 17:06:27 +0100

    gpu3 works with multiples of 2 as block size, but grid size can be anything

--- gpu3/gpu3.cuh
+++ gpu3/gpu3.cuh
@@ -4,12 +4,10 @@
 #include <stdint.h>
 #include "../common/lspbmp.hpp"
 
-void and_reduction(uint8_t* g_data, int g_width, int g_height, dim3 grid_dim, dim3 block_dim);
-__global__ void and_reduction(uint8_t* g_data, int g_width, int g_height);
+void and_reduction(uint8_t* g_data, int g_size, dim3 grid_dim, dim3 block_dim);
+__global__ void and_reduction(uint8_t* g_data, int g_size);
 __device__ uint8_t black_neighbors_around(uint8_t* s_data, int s_row, int s_col, int s_width);
 __device__ uint8_t block_and_reduce(uint8_t* s_data);
-__device__ uint8_t global_mem_read(uint8_t* g_data, int g_row, int g_col, int g_width, int g_height);
-__device__ void global_mem_write(uint8_t* g_data, int g_row, int g_col, int g_width, int g_height, uint8_t write_data);
 __device__ uint8_t is_outside_image(int g_row, int g_col, int g_width, int g_height);
 __device__ void load_s_src(uint8_t* g_src, int g_row, int g_col, int g_width, int g_height, uint8_t* s_src, int s_row, int s_col, int s_width);
 __device__ uint8_t P2_f(uint8_t* s_data, int s_row, int s_col, int s_width);
@@ -20,7 +18,9 @@ __device__ uint8_t P6_f(uint8_t* s_data, int s_row, int s_col, int s_width);
 __device__ uint8_t P7_f(uint8_t* s_data, int s_row, int s_col, int s_width);
 __device__ uint8_t P8_f(uint8_t* s_data, int s_row, int s_col, int s_width);
 __device__ uint8_t P9_f(uint8_t* s_data, int s_row, int s_col, int s_width);
-__global__ void pixel_equality(uint8_t* g_in_1, uint8_t* g_in_2, uint8_t* g_out, int g_width, int g_height);
+__global__ void pixel_equality(uint8_t* g_in_1, uint8_t* g_in_2, uint8_t* g_out, int g_size);
+__device__ uint8_t safe_global_mem_read(uint8_t* g_data, int g_row, int g_col, int g_width, int g_height);
+__device__ void safe_global_mem_write(uint8_t* g_data, int g_row, int g_col, int g_width, int g_height, uint8_t write_data);
 int skeletonize(Bitmap** src_bitmap, Bitmap** dst_bitmap, dim3 grid_dim, dim3 block_dim);
 __global__ void skeletonize_pass(uint8_t* g_src, uint8_t* g_dst, int g_width, int g_height);
 __device__ uint8_t wb_transitions_around(uint8_t* s_data, int s_row, int s_col, int s_width);
