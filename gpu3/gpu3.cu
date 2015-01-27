aee43861d86114dfc47b7a4ccda735540e8f85cb
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-01-27 17:58:38 +0100

    removed border_global_mem_write from gpu3, because never really used. gpu3 doesn't work

--- gpu3/gpu3.cu
+++ gpu3/gpu3.cu
@@ -91,12 +91,6 @@ __device__ uint8_t border_global_mem_read(uint8_t* g_data, int g_row, int g_col,
     return is_outside_image(g_row, g_col, g_width, g_height) ? BINARY_WHITE : g_data[g_row * g_width + g_col];
 }
 
-__device__ void border_global_mem_write(uint8_t* g_data, int g_row, int g_col, int g_width, int g_height, uint8_t write_data) {
-    if (!is_outside_image(g_row, g_col, g_width, g_height)) {
-        g_data[g_row * g_width + g_col] = write_data;
-    }
-}
-
 __device__ uint8_t is_outside_image(int g_row, int g_col, int g_width, int g_height) {
     return (g_row < 0) | (g_row > (g_height - 1)) | (g_col < 0) | (g_col > (g_width - 1));
 }
@@ -276,7 +270,7 @@ __global__ void skeletonize_pass(uint8_t* g_src, uint8_t* g_dst, int g_width, in
         uint8_t thinning_cond_ok = thinning_cond_1 & thinning_cond_2 & thinning_cond_3 & thinning_cond_4;
 
         uint8_t g_dst_next = (thinning_cond_ok * BINARY_WHITE) + ((1 - thinning_cond_ok) * s_src[s_row * s_width + s_col]);
-        border_global_mem_write(g_dst, g_row, g_col, g_width, g_height, g_dst_next);
+        g_dst[g_row * g_width + g_col] = g_dst_next;
 
         tid += (gridDim.x * blockDim.x);
     }
