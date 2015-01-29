a9b8b76093a214f3aabf82eab247a755fd20299c
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-01-29 09:49:55 +0100

    generate binary named "cpu_normal" instead of "cpu"

--- cpu/normal/compile.sh
+++ cpu/normal/compile.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR"
 
-g++ -g -O3 cpu.cpp cpu_only_utils.cpp ../../common/lspbmp.cpp ../../common/utils.cpp -o ../../../bin/cpu
+g++ -g -O3 cpu.cpp cpu_only_utils.cpp ../../common/lspbmp.cpp ../../common/utils.cpp -o ../../../bin/cpu_normal
